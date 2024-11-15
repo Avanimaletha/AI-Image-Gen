5a1e78851212d0833b071650cdb1a0d801ae548a
Avanimaletha <125181112+Avanimaletha@users.noreply.github.com>
2024-11-16 03:23:32 +0530

    gg

---
+++ src/components/image-gen/ImageGen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './imageGen.css';
+import default_image from './assets/image.jpeg';
+
+const ImageGen = () => {
+  return (
+    <div className="ai-image-generator">
+      <div className="header">AI-image<span>Generator</span></div>
+    </div>
+  );
+};
+
+export default ImageGen;
